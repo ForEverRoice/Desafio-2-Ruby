625ec9c61637e8fd28f1cb055de95bd8bcc7aeea
ForEverRoice <dasilva1807@hotmail.com>
2019-04-08 18:49:54 -0400

    resto ok, duda en mayor de 4

--- mayor_de_4.rb
+++ mayor_de_4.rb
@@ -1,22 +1,34 @@
-valor1 = ARGV[0]
-valor1 = valor1.to_f
-valor2 = ARGV[1]
-valor2 = valor2.to_f
-valor3 = ARGV[2]
-valor3 = valor3.to_f
-valor4 = ARGV[3]
-valor4 = valor4.to_f
+valor1 = ARGV[0].to_f
+valor2 = ARGV[1].to_f
+valor3 = ARGV[2].to_f
+valor4 = ARGV[3].to_f
 
-if valor1 > valor2 and valor1 > valor3 and valor1 > valor4
-  puts "Este valor es el mayor #{valor1}"
 
-  elsif valor2 > valor1 and valor2 > valor3 and valor2 > valor4
+if ARGV[3]==nil
+
+  valor1 > valor2 and valor1 > valor3
+    puts "Este valor es el mayor #{valor1}"
+    elsif valor2 > valor1 and valor2 > valor3
       puts "Este valor es mayor #{valor2}"
-
-    elsif valor3 > valor1 and valor3 > valor2 and valor3 > valor4
+    else
+      valor3 > valor1 and valor3 > valor2
       puts "Este valor es mayor #{valor3}"
 
-  else
-    valor4 > valor1 and valor4 > valor2 and valor4 > valor3
-    puts "Este valor es mayor #{valor4}"
+end
+
+
+if ARGV[3]!=nil
+
+  valor1 > valor2 and valor1 > valor3 and valor1 > valor4
+    puts "Este valor es el mayor #{valor1}"
+
+    elsif valor2 > valor1 and valor2 > valor3 and valor2 > valor4
+        puts "Este valor es mayor #{valor2}"
+
+      elsif valor3 > valor1 and valor3 > valor2 and valor3 > valor4
+        puts "Este valor es mayor #{valor3}"
+
+    else
+      valor4 > valor1 and valor4 > valor2 and valor4 > valor3
+      puts "Este valor es mayor #{valor4}"
 end
